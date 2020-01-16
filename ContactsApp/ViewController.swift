c73129cb90e57e26e4c7867a301bcb37c2b3e56b
Mohammed Sadiq <mohammad.sadiq@gonuclei.com>
2020-01-16 12:31:44 +0530

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Mohammed Sadiq on 16/01/20.
+//  Copyright © 2020 Mohammed Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
